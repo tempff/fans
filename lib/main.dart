87bb5c4c67b7f0fdda91faa3eb3fe49c64cffa65
nikhilpanseriya1 <nikhil.panseriya1@gmail.com>
2022-08-12 10:28:05 +0530

    commit_code

--- lib/main.dart
+++ lib/main.dart
@@ -56,8 +56,6 @@ class MyApp extends StatelessWidget {
             // ,
             // themeMode: themeProvider.themeMode,
             // darkTheme: MyTheme.darkTheme,
-
-
             builder: (context, widget) => ResponsiveWrapper.builder(
               BouncingScrollWrapper.builder(context, widget!),
               maxWidth: MediaQuery.of(context).size.width,
